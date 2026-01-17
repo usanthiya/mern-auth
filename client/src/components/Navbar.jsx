af916c96eb05a6eb938e83f141282cc887be197b
usanthiya <santhiyaudhya1@gmail.com>
2026-01-17 18:23:09 +0530

    feat: Implement logout functionality in Navbar and enhance AppContext for user authentication

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,19 +2,60 @@ import React from "react";
 import logo from "../assets/logo.png";
 import { ArrowRight } from "lucide-react";
 import { useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { AppContext } from "../context/AppContext";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 const Navbar = () => {
   const navigate = useNavigate();
+  const { userData, backendUrl, setIsLoggedin, setUserData } = useContext(AppContext);
   
+  const logout = async() => {
+
+    /**
+     * Tells Axios to:
+      Send cookies with every HTTP request
+      Accept cookies from the backend
+      Work across CORS (cross-origin requests)
+
+      Without this, cookies are ignored by the browser
+     */
+    axios.defaults.allowCredentials = true; 
+
+    try{
+      const { data } = await axios.post(`${backendUrl}/api/auth/logout`);
+      if(data.success){
+        setIsLoggedin(false);
+        setUserData(null);
+      }else{
+        toast.error(data.message);
+      }
+    }catch(err){
+      toast.error(err.message);
+    }
+  }
   return (
     <div className="w-full flex justify-between items-center p-4 sm:p-6 sx:px-24 absolute top-0">
       <img src={logo} alt="Logo" className="h-10" />
-      <button
-        onClick={() => navigate("/login")}
-        className="flex items-center gap-2 border border-gray-500 rounded-full px-6 py-2 text-gray-800 hover:bg-gray-100 transition-all"
-      >
-        Login <ArrowRight size={16} />
-      </button>
+      {userData ? (
+        <div className="h-8 w-8 flex items-center justify-center rounded-full bg-black text-white relative group cursor-pointer">
+          {userData.name[0].toUpperCase()}
+          <div className="absolute hidden group-hover:block top-0 right-0 z-10 rounded text-black pt-10">
+            <ul className="list-none m-0 p-2 bg-gray-100 text-sm">
+              {!userData.isAccountVerified && <li className="py-1 px-2 hover:bg-gray-200">Verify Email</li>}
+              <li onClick={()=> logout()}className="py-1 px-2 hover:bg-gray-200 pr-10">Logout</li>
+            </ul>
+          </div>
+        </div>
+      ) : (
+        <button
+          onClick={() => navigate("/login")}
+          className="flex items-center gap-2 border border-gray-500 rounded-full px-6 py-2 text-gray-800 hover:bg-gray-100 transition-all"
+        >
+          Login <ArrowRight size={16} />
+        </button>
+      )}
     </div>
   );
 };
--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,22 +1,41 @@
 import { createContext, useState } from "react";
-import axios from 'axios';
-import { toast } from 'react-toastify';
+import axios from "axios";
+import { toast } from "react-toastify";
+import { useEffect } from "react";
 
 export const AppContext = createContext();
 
 export const AppContextProvider = (props) => {
   let backendUrl = import.meta.env.VITE_BACKEND_URL;
   const [isLoggedin, setIsLoggedin] = useState(false);
-  const [userData, setUserData] = useState(false);
+  const [userData, setUserData] = useState();
 
-  const getUserData = async() => {
-    try{
-      const { data } = await axios.get(`${backendUrl}/api/user/data`);
-      data.success ? setUserData(data.userData) : toast.error(data.message);
-    }catch(err){
+  const getAuthState = async () => {
+    try {
+      const { data } = await axios.get(`${backendUrl}/api/auth/is-auth`);
+      if (data.success) {
+        setIsLoggedin(true);
+        getUserData();
+      } else {
+        toast.error(data.message);
+      }
+    } catch (err) {
       toast.error(err.message);
     }
-  }
+  };
+
+  useEffect(()=>{
+    getAuthState();
+  },[])
+
+  const getUserData = async () => {
+    try {
+      const { data } = await axios.get(`${backendUrl}/api/user/data`);
+      data.success ? setUserData(data.userData) : toast.error(data.message);
+    } catch (err) {
+      toast.error(err.message);
+    }
+  };
 
   const value = {
     backendUrl,
@@ -24,12 +43,11 @@ export const AppContextProvider = (props) => {
     setIsLoggedin,
     userData,
     setUserData,
-    getUserData
+    getUserData,
+    getAuthState
   };
 
   return (
-    <AppContext.Provider value={value}>
-      {props.children}
-    </AppContext.Provider>
+    <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
   );
 };
