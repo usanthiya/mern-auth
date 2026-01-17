2cc3378bb184565eaa336b5a11b077b4f215249d
usanthiya <santhiyaudhya1@gmail.com>
2026-01-17 18:23:40 +0530

    ref:: Change is-auth route method from POST to GET

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -8,7 +8,7 @@ router.route('/login').post(login);
 router.route('/logout').post(logout);
 router.route('/send-verify-otp').post(userAuth, sendVerifyOtp);
 router.route('/verify-account').post(userAuth, verifyEmail);
-router.route('/is-auth').post(userAuth, isAuthenticated);
+router.route('/is-auth').get(userAuth, isAuthenticated);
 router.route('/send-reset-otp').post(sendResetOtp);
 router.route('/reset-password').post(resetPassword);
 
